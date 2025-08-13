9b68e7f2f07b22580f6f3d09389b2a8d158e9168
technocert.adm <technocert.adm@gmail.com>
2025-08-13 11:21:46 +0900

    updated

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                             <h2 class="card-title">（0➡1）システムビジネスモデリング</h2>
                             <p class="card-text">デジタル技術を核とするビジネスをモデリングし、その情報システム化を支援します。また必要に応じてそのシステムの差異化要素に関するプロト設計を実施し、お客様のPoCプロセス推進に取り組みます。</p>
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="service.php?type=modeling">More Info</a></div>
                     </div>
                 </div>
                 <div class="col-md-6 mb-5">
@@ -60,7 +60,7 @@
                             <h2 class="card-title">（1➡10）プロジェクトマネジメント</h2>
                             <p class="card-text">システム設計におけるプロジェクト計画策定、要求要件管理、および進捗管理を通じてプロジェクトの成功率向上に寄与します。</p>
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="service.php?type=management">More Info</a></div>
                     </div>
                 </div>
             </div>
--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,7 @@
-<div class="row gx-4 gx-lg-5">
-    <div class="col-md-6 mb-5">
-        <div class="card h-100">
-            <div class="card-body">
-                <h2 class="card-title">（0➡1）システムビジネスモデリング</h2>
-                <p class="card-text">デジタル技術を核とするビジネスをモデリングし、その情報システム化を支援します。また必要に応じてそのシステムの差異化要素に関するプロト設計を実施し、お客様のPoCプロセス推進に取り組みます。</p>
-            </div>
-            <div class="card-footer"><a class="btn btn-primary btn-sm" href="service.php?type=modeling">More Info</a></div>
-        </div>
-    </div>
-    <div class="col-md-6 mb-5">
-        <div class="card h-100">
-            <div class="card-body">
-                <h2 class="card-title">（1➡10）プロジェクトマネジメント</h2>
-                <p class="card-text">システム設計におけるプロジェクト計画策定、要求要件管理、および進捗管理を通じてプロジェクトの成功率向上に寄与します。</p>
-            </div>
-            <div class="card-footer"><a class="btn btn-primary btn-sm" href="service.php?type=management">More Info</a></div>
-        </div>
-    </div>
-</div>
+/*!
+* Start Bootstrap - Small Business v5.0.1 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
